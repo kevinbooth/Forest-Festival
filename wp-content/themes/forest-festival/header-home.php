b1c5b88cb04e191d11a5265d648664182afe1161
Kevin Booth <kdbooth97@gmail.com>
2018-08-20 20:57:34 -0400

    Add homepage header and front page

---
+++ wp-content/themes/forest-festival/header-home.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The homepage header and navigation
+ *
+ */
+
+?>
+
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="/wp-content/themes/forest-festival/styles/styles.min.css">
+
+	
+</head>
+
+<body>
+    <header class="header header--desktop">
+        <div class="container">
+            <div class="row">
+                <div class="two columns">
+                    <img src="/wp-content/themes/forest-festival/images/logo.png" />
+                </div>
+                <div class="ten columns"> 
+
+                </div>
+            </div>
+        </div>
+    </header>
