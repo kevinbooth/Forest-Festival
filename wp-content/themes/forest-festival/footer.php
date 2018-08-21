1a4d0231db6029ef51ae3b71782f605657e5eedd
Kevin Booth <kdbooth97@gmail.com>
2018-08-20 22:32:37 -0400

    Update homepage header and hero section

--- wp-content/themes/forest-festival/footer.php
+++ wp-content/themes/forest-festival/footer.php
@@ -10,25 +10,9 @@
  */
 
 ?>
+	<footer>
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'forest-festival' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'forest-festival' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'forest-festival' ), 'forest-festival', '<a href="https://github.com/kevinbooth">Kevin Booth</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+	</footer>
 
 <?php wp_footer(); ?>
 
--- wp-content/themes/forest-festival/header-home.php
+++ wp-content/themes/forest-festival/header-home.php
@@ -17,6 +17,24 @@
 </head>
 
 <body>
+    <header class="header header--mobile">
+        <div class="container">
+            <div class="row">
+                <div class="twelve columns">
+                    <div class="header__mobile-content">
+                        <a href="/" class="header__logo">
+                            <img src="/wp-content/themes/forest-festival/images/logo.png" width="40" height="40"/>
+                        </a>
+                        <div class="header__burger">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>    
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
     <header class="header header--desktop">
         <div class="container">
             <div class="row">
@@ -29,3 +47,12 @@
             </div>
         </div>
     </header>
+    <div class="section section--hero">
+        <div class="container">
+            <div class="row">
+                <div class="twelve columns">
+                    <div class="hero__copy">affordable, rubust web development by kevin booth</div>
+                </div>
+            </div>
+        </div>
+    </div>
